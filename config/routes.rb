555234f7275a1833eaf32a0b3e8e56126e0ced23
kashish garg <kashishgarg27@gmail.com>
2018-03-01 07:55:53 -0500

    application updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  get 'pages/home', to: 'pages#home'
-  get 'pages/about', to: 'pages#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  
+  resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
